1a82a2096edd41c9265b75e9cd38fc5a2d0380d6
Unumongmarmahub <unumongmarma1998@gmail.com>
2025-01-02 21:03:30 +0600

    add some blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl bg-gray-800">React Knowledge World Cafe </h1>
+      <Header></Header>
+      <main className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </main>
     </>
   );
 }
